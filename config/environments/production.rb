adffe3885b61f8f0e89b3bba557e921773a54f1b
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-02-27 09:28:48 +0900

    Update production.rb config.assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # git push heroku Precompiling assets failed.
+  config.assets.initialize_on_precompile = false
 end
